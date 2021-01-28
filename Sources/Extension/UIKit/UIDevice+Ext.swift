5e0760b785c533e9abf537892de34b3fe28073d8
SoalHuang <sosoniceme@163.com>
2021-01-28 17:43:24 +0800

    update device extension

--- Sources/Extension/UIKit/UIDevice+Ext.swift
+++ Sources/Extension/UIKit/UIDevice+Ext.swift
@@ -11,11 +11,20 @@ import SDFoundation
 
 public extension SDExtension where T: UIDevice {
     
-    static var isIPhone4:   Bool { return Int(max(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)) == 480 }
-    static var isIPhone5:   Bool { return Int(max(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)) == 568 }
-    static var isIphone6:   Bool { return Int(max(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)) == 667 }
-    static var isIphone6p:  Bool { return Int(max(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)) == 736 }
-    static var isIPhoneX:   Bool { return Int(max(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)) == 812 }
-    static var isIPhoneMX:  Bool { return Int(max(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)) == 896 }
-    static var isXGroup:    Bool { return isIPhoneX || isIPhoneMX }
+    static var isIPhone4:       Bool { DeviceIsiPhone4 }
+    
+    static var isIPhone5:       Bool { DeviceIsiPhone5 }
+    
+    static var isIphone6:       Bool { DeviceIsiPhone6 }
+    static var isIphone6p:      Bool { DeviceIsiPhone6p }
+    
+    static var isIPhoneX:       Bool { DeviceIsiPhoneX }
+    static var isIPhoneXR:      Bool { DeviceIsiPhoneXR }
+    static var isIPhoneMX:      Bool { DeviceIsiPhoneMX }
+    
+    static var isIPhone12:      Bool { DeviceIsiPhone12 }
+    static var isIPhone12Mini:  Bool { DeviceIsiPhone12Mini }
+    static var isIPhone12Pro:   Bool { DeviceIsiPhone12Pro }
+    
+    static var isXGroup:        Bool { DeviceIsXGroup }
 }
